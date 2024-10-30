20207407885abe4bcdbc2e6110df3987155a8cf1
Sumaita Bhuiyan Parveen <sumaita0503@gmail.com>
2024-10-30 12:37:48 +0000

    :))

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -1,19 +1,19 @@
 #Script to estimate the model parameters using a linear approximation
 
-#library(dplyr)
+library(dplyr)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<1300) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>2500)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,7 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+library(ggplot2)
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +11,18 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- exp(6.889e+00) #initial size of population intercept from the 1st dataset
   
-r <- ??? #
+r <- 1.002e-02 #growth rate t from first dataset
   
-K <- ??? #
+K <-  6.00e+10 #carrying capacity intercept for 2nd dataset
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
   geom_point()
 
-  #scale_y_continuous(trans='log10')
+  scale_y_continuous(trans='log10')
 
 
